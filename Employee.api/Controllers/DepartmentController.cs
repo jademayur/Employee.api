010521d474ed328005cf9fc5f1d0f7e04b719123
Mayur Jade <jademayur@gmail.com>
2026-01-19 17:40:42 +0530

    minor changes

--- Employee.api/Controllers/DepartmentController.cs
+++ Employee.api/Controllers/DepartmentController.cs
@@ -7,6 +7,18 @@ namespace Employee.api.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
+        private readonly Model.AppDbContext _context;
+        public DepartmentController(Model.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllDepartments()
+        {
+            var departments = _context.Departments.ToList();
+            return Ok(departments);
+        }
 
     }
 }
